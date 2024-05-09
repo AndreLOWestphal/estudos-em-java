a7108274185a98096c62c9ed332a7ff8a7d2bf3b
AndreLOWestphal <dedewestphal@hotmail.com>
2024-05-09 18:37:55 -0300

    Adicionando sistema para TV, ligar, desligar, volume e canal

---
+++ MEU-PROJETO/src/edu/basico/SmartTV.java
@@ -0,0 +1,36 @@
+package edu.basico;
+
+public class SmartTV {
+    boolean ligada = false;
+    int canal = 4;
+    int volume = 20;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Volume: " + volume);
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Volume: " + volume);
+    }
+
+    public void aumentarCanal(){
+        canal += 2;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void diminuirCanal(){
+        canal -= 2;
+    }
+}
---
+++ MEU-PROJETO/src/edu/basico/UsuarioTV.java
@@ -0,0 +1,27 @@
+package edu.basico;
+
+public class UsuarioTV {
+    
+    public static void main(String[] args) {
+        
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.ligar();
+        smartTV.aumentarCanal();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+
+        System.out.println("A TV está ligada: " + smartTV.ligada);
+        System.out.println("Canal " + smartTV.canal);
+        System.out.println("Volume " + smartTV.volume);
+
+        smartTV.mudarCanal(50);
+
+        System.out.println("Canal " + smartTV.canal);
+
+
+    }
+
+}
