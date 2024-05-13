fc9ee251da831e3d2c08cf7ba6483325b9d08374
AndreLOWestphal <dedewestphal@hotmail.com>
2024-05-13 11:06:56 -0300

    Organizando pastas

---
+++ src/edu/basico/NovoFormulario.java
@@ -0,0 +1,29 @@
+package edu.basico;
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class NovoFormulario {
+    
+    public static void main(String[] args) { 
+    
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite seu nome");
+    String nome = scanner.next();
+
+    System.out.println("Digite seu sobrenome");
+    String sobrenome = scanner.next();
+
+    System.out.println("Digite sua idade");
+    int idade = scanner.nextInt();
+
+    System.out.println("Digite sua altura");
+    double altura = scanner.nextDouble();
+
+    System.out.println("Olá meu nome é " + nome + " " + sobrenome + ".");
+    System.out.println("Tenho " + idade + " anos.");
+    System.out.println("Minha altura é " + altura + " cm.");
+
+}
+}
